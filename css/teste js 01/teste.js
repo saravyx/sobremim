aeda23b43b03bc742081b5e9bcf7aea4c37976c5
Sara Vitória <saravitoriaribeiro1819@gmail.com>
2026-07-18 16:31:35 -0300

    Add files via upload

---
+++ css/teste js 01/teste.js
@@ -0,0 +1,79 @@
+//let idade = prompt("qual é a sua idade?")
+//if (idade > 18){
+//  console.log("maior de idade")
+//}else{
+//console.log("menor de idade")
+//}
+
+//let numero = prompt("digíte o número")
+//if(numero > 0){
+//console.log("número é positivo")}
+//else if (numero < 0) {
+//console.log("número é negativo")}
+//else {
+//console.log("o número é zero")
+//}
+
+//let num= 1
+//let controle = 1
+//let rep = 8500
+
+//while (rep>=controle){
+// //if (num % 2 ==0)
+//console.log(num + "é par")
+//num++
+//controle++}
+//if (num % 2 != 0){
+///console.log(num+ "é ímpar")
+//num++
+//controle++
+//}
+
+
+//let comida = batata
+//for (let batata=0; batata< 8500; batata++){
+//batata%2==0
+//console.log(comida);
+//} 
+
+/* questao 1 
+let n1 = prompt("Digite o primeiro número");
+let n2 = prompt("Digite o segundo número");
+
+if (n1>n2) {
+    alert(n1)
+    console.log("maior");
+} else {
+    alert (n2)
+    console.log("menor");
+ 
+}*/
+
+
+/* questao 2 
+let nota = prompt("Digíte a sua nota")
+
+if (nota => 6){
+    console.log("Aprovado")
+}else{
+    console.log("Reprovado")
+}*/
+
+
+/* questao 3 
+let numero = prompt("Digíte um número")
+
+if(numero % 2 == 0){
+    console.log("par")
+}else{
+    console.log("ímpar")
+}*/
+
+
+/*questão 5*/
+let numero = prompt("Dígite um número")
+while(numero>=0){
+    console.log("numero");
+    numero--;
+}
+
